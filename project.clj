616b2a2e3be5aace20f6900257155953949d8756
Eric Scott <eric.scott@acm.org>
2021-12-19 06:48:34 -0800

    Updating deps with known security issues/fixes per NVD

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "https://github.com/ont-app/igraph-grafter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;; disambiguate deps :tree
+  :dependencies [;; Per 'lein nvd check':
+                 [commons-beanutils "1.9.4"]
+                 [org.apache.commons/commons-compress "1.21"]
+                 [commons-io "2.11.0"]
+                 ;; NOTE: Issues CVE-2018-10237, CVE-2020-8908 (medium risk) still not addressed in latest guava (23.0)
+                 ;; NOTE: Issue CVE-2020-13956 (medium risk) still not addressed in latest httpclient (4.5.13)
+                 ;; disambiguate deps :tree
                  [org.clojure/spec.alpha "0.3.218"]
                  [cheshire "5.10.1"]
                  ;; clojure core
